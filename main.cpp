74511fb6fcf1445b6acec244b736de6534f156fa
Cristi104 <petrecristian2004@gmail.com>
2024-06-12 00:34:14 +0300

    GLFW test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include "GLFW/glfw3.h"
+#include "../include/Window.h"
 
 int main(){
-    std::cout << "Hello, world!";
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "test", nullptr, nullptr);
+    if(window == nullptr){
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glViewport(0,0,800,600);
+
+    std::cout << "Hello, World!\n";
     return 0;
 }
